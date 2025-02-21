fc888234177826d17e8cd8c0c6b4f646bafc5a55
nikolinak1 <niki.konatarevic@gmail.com>
2025-02-21 11:29:20 +0100

    chore: added Volumes to docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,13 @@
 echo "Waiting for the database to be available..."
 
 while ! nc -z db 5432; do
-
   sleep 0.1
 done
 
 echo "Database is up - executing command"
 
 # Start the FastAPI server
-if [ "${RELOAD}" = "True" ]; then
+if [ "${LOCAL}" = "True" ]; then
     uvicorn src.main:app --host 0.0.0.0 --reload
 else
     uvicorn src.main:app --host 0.0.0.0
