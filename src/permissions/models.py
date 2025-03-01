ee30200f455f657cc1c4f4ffd14a7d374ac01272
nikolinak1 <niki.konatarevic@gmail.com>
2025-03-01 23:25:49 +0100

    chore(permissions): update models and enum.py

--- src/permissions/models.py
+++ src/permissions/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Enum, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.testing.schema import mapped_column
 
-from src.dataBase.base_model import Base
+from src.data_base.base_model import Base
 from src.permissions.enums import RequestStatus, UserRole
 from src.projects.models import Project
 from src.users.models import User
@@ -11,8 +11,8 @@ from src.users.models import User
 class Permission(Base):
     __tablename__ = "permissions"
 
-    project_id: Mapped[int] = mapped_column(Integer, ForeignKey("project_id"), nullable=False, comment="Project ID")
-    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("user_id"), nullable=False, comment="User ID")
+    project_id: Mapped[int] = mapped_column(Integer, ForeignKey("projects.id"), nullable=False, comment="Project ID")
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False, comment="User ID")
     user_role: Mapped[UserRole] = mapped_column(Enum(UserRole), nullable=False, comment="User Role")
     request_status: Mapped[RequestStatus] = mapped_column(Enum(RequestStatus), nullable=False, comment="Request Status")
 
