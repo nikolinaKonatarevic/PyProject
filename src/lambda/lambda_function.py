9ec04646730d158a3119fa2b132bd93c80964b25
nikolinaKonatarevic <niki.konatarevic@gmail.com>
2025-03-17 12:21:54 +0100

    fix: fix CD

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -10,22 +10,22 @@ class S3Client:
         self.client = boto3.client("s3")
 
     def upload(self, doc, file_path: str) -> str:
-        self.client.upload_fileobj(doc.file, os.environ['AWS_BUCKET_NAME'], f'{file_path}/{doc.filename}')
+        self.client.upload_fileobj(doc.file, os.environ['BUCKET_NAME'], f'{file_path}/{doc.filename}')
 
         url = (
-            f"https://{os.environ['AWS_BUCKET_NAME']}.s3"
-            f".{os.environ['AWS_DEFAULT_REGION']}.amazonaws.com/{file_path}/{doc.filename}"
+            f"https://{os.environ['BUCKET_NAME']}.s3"
+            f".{os.environ['DEFAULT_REGION']}.amazonaws.com/{file_path}/{doc.filename}"
         )
         return url
 
     def download(self, file_name: str, file_path: str, download_path: str):
         result = self.client.download_file(
-            Bucket=os.environ["AWS_BUCKET_NAME"], Key=f"{file_path}/{file_name}", FileName=download_path
+            Bucket=os.environ["BUCKET_NAME"], Key=f"{file_path}/{file_name}", FileName=download_path
         )
         return result
 
     def delete(self, file_name: str, file_path: str) -> None:
-        self.client.delete_object(Bucket=os.environ["AWS_BUCKET_NAME"], Key=f"{file_path}/{file_name}")
+        self.client.delete_object(Bucket=os.environ["BUCKET_NAME"], Key=f"{file_path}/{file_name}")
 
 
 def lambda_handler(event, context):
