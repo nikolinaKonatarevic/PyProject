7731d1ec61d4e972c43e540423e900a169d0177e
nikolinaKonatarevic <niki.konatarevic@gmail.com>
2025-03-25 10:31:34 +0100

    fix: fix lambda

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -16,17 +16,20 @@ class S3Client:
 
         self.client.upload_file(f"{download_path}/{file_name}", os.environ['BUCKET_NAME'], f'{new_path}/{file_name}')
 
-        url = (
-            f"https://{os.environ['BUCKET_NAME']}.s3"
-            f".{os.environ['DEFAULT_REGION']}.amazonaws.com/{new_path}/{file_name}"
-        )
-        return url
 
     def download(self, file_name: str, file_path: str, download_path: str):
         print (f"download {file_path}  +++  {file_name} ++ {download_path}")
-        self.client.download_file(
-            Bucket=os.environ["BUCKET_NAME"], Key=f"{file_path}/{file_name}", Filename= download_path)
-        print("downloading ended")
+        # Get object from S3
+        response = self.client.get_object(
+            Bucket=os.environ["BUCKET_NAME"],
+            Key=f"{file_path}/{file_name}"
+        )
+
+        # Read file content and save it locally
+        with open(download_path, "wb") as f:
+            f.write(response["Body"].read())
+
+        print("Download complete.")
 
 
     def delete(self, file_name: str, file_path: str) -> None:
@@ -43,13 +46,16 @@ def lambda_handler(event, context):
         [file_path, file_name] = key.split("/")
         print (f"{file_path}  +++  {file_name}")
         tempfile = f"documents/{file_name}"
-        s3_client.download(file_name, file_path, download_path=tempfile)
 
-        s3_client.delete(file_name, file_path)
+        s3_client.download(file_name, file_path, download_path=tempfile)
 
         with Image.open(tempfile) as img:
             img.thumbnail((400, 400))
             print(f"save photo")
             img.save(tempfile)
+
         print("before last upload")
+
         s3_client.upload(file_name, "documents", "proccessed")
+
+        s3_client.delete(file_name, file_path)
