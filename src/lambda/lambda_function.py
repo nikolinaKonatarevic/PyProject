2d16b0a4160a8c7dde81e771bd6c4231db9687c5
nikolinaKonatarevic <niki.konatarevic@gmail.com>
2025-03-25 11:06:23 +0100

    fix: fix lambda

--- src/lambda/lambda_function.py
+++ src/lambda/lambda_function.py
@@ -9,26 +9,26 @@ class S3Client:
     def __init__(self):
         self.client = boto3.client("s3")
 
-    def upload(self, file_name:str,download_path: str, new_path: str) -> str:
+    def upload(self, file_name:str, download_path: str, new_path: str):
         print(f"in the upload - {file_name} +++ {download_path} +++ {new_path}")
 
-        self.download(file_name, download_path,new_path)
+        self.download(file_name, download_path, new_path)
 
         self.client.upload_file(f"{download_path}/{file_name}", os.environ['BUCKET_NAME'], f'{new_path}/{file_name}')
 
 
-    def download(self, file_name: str, file_path: str, download_path: str):
-        print (f"download {file_path}  +++  {file_name} ++ {download_path}")
+    def download(self, file_name: str, file_path: str, new_path: str):
+        print (f"download {file_path}  +++  {file_name} ++ {new_path}")
         # Get object from S3
         response = self.client.get_object(
             Bucket=os.environ["BUCKET_NAME"],
             Key=f"{file_path}/{file_name}"
         )
 
-        os.makedirs(os.path.dirname(download_path), exist_ok=True)
+        os.makedirs(os.path.dirname(new_path), exist_ok=True)
 
         # Read file content and save it locally
-        with open(download_path, "wb") as f:
+        with open(new_path, "wb") as f:
             f.write(response["Body"].read())
 
         print("Download complete.")
@@ -47,9 +47,9 @@ def lambda_handler(event, context):
 
         [file_path, file_name] = key.split("/")
         print (f"{file_path}  +++  {file_name}")
-        tempfile = f"documents/{file_name}"
+        tempfile = f"/tmp/{file_name}"
 
-        s3_client.download(file_name, file_path, download_path=tempfile)
+        s3_client.download(file_name, file_path, new_path=tempfile)
 
         with Image.open(tempfile) as img:
             img.thumbnail((400, 400))
@@ -58,6 +58,6 @@ def lambda_handler(event, context):
 
         print("before last upload")
 
-        s3_client.upload(file_name, "documents", "proccessed")
+        s3_client.upload(file_name, "/tmp", "proccessed")
 
         s3_client.delete(file_name, file_path)
